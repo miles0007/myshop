e2d7b19fa5f4d8ef7dfc9833ff46eabe4f5a94ab
miles007 <milesstonner@gmail.com>
2020-07-09 13:31:26 +0530

    Admin Changes

--- shop/admin.py
+++ shop/admin.py
@@ -7,14 +7,18 @@ class CategoryAdmin(admin.ModelAdmin):
 	list_display = ('name','slug')
 	prepopulated_fields = {'slug':('name',)}
 
+class ImageInline(admin.StackedInline):
+	model = SlideImage
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
 	list_display = ('name','slug','price','available','created','updated')
 	list_filter = ['available','created','updated']
 	list_editable = ['price','available']
 	prepopulated_fields = {'slug':('name',)}
+	inlines = [ImageInline]
 
-@admin.register(SlideImage)
-class ImagesAdmin(admin.ModelAdmin):
-	list_display = ('name','product')
-	search_fields = ('name',)
+# @admin.register(SlideImage)
+# class ImagesAdmin(admin.ModelAdmin):
+# 	list_display = ('name','product')
+# 	search_fields = ('name',)
